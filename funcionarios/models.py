9985842bdd64294a474de21cb610fa9691d75c6b
Luanna Siqueira <153955937+holandalhs@users.noreply.github.com>
2025-01-22 10:58:15 -0300

    feat: vincular models ao admin para app funcionários

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
@@ -9,12 +10,14 @@ class Categoria(models.Model):
 
 
 class Funcionario(models.Model):  
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    ##chave estrangeira empresa vinculada ao funcionário
     nome = models.CharField(max_length=100)  
     cargo = models.CharField(max_length=100)  
     salario = models.DecimalField(max_digits=10, decimal_places=2)  
     data_admissao = models.DateField()  
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    ##chave estrangeira empresa vinculada ao funcionário
+    categoria_senioridade = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    carta_apresentacao = models.TextField(blank=True, null=True)
 
     def __str__(self):  
         return self.nome
